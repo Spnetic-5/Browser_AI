2735bb007197d30628b22928ced9026c28b76b3e
Spnetic-5 <sspowar_b19@el.vjti.ac.in>
2023-07-24 11:22:49 -0400

    🐛 FIX: firebase config env

--- browserai_app/src/server.js
+++ browserai_app/src/server.js
@@ -6,13 +6,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvtHZVKBeY6btKmkg3AzfrjTLtJkBW89M",
-  authDomain: "browserai.firebaseapp.com",
-  projectId: "browserai",
-  storageBucket: "browserai.appspot.com",
-  messagingSenderId: "607449644721",
-  appId: "1:607449644721:web:93de34b8c823893b0aac90",
-  measurementId: "G-BFXWGXSZHG"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
